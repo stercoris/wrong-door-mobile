f40ea1a55b4c6a408cefd176a388130b44dbf3e0
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2020-10-20 21:54:19 +0400

    GetOutput

--- app/src/main/java/com/example/mycollege/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mycollege/ui/home/HomeFragment.kt
@@ -13,7 +13,6 @@ import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.mycollege.*
